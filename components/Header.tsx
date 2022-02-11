792ce0dcfb7c2e50172465045615ea65cf1d45ce
AayushMohan <aayushmohan1702@gmail.com>
2022-02-11 17:53:11 +0530

    Added Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { FiArrowUpRight } from 'react-icons/fi'
+import { AiOutlineDown } from 'react-icons/ai'
+import { HiOutlineDotsVertical } from 'react-icons/hi'
+import ethLogo from '../assets/eth.png'
+import uniswapLogo from '../assets/uniswap.png'
+import { useContext } from 'react'
+// import { TransactionContext } from '../context/TransactionContext'
+// import { client } from '../lib/sanityClient'
+
+export const Header = () => {
+  return <div>Header</div>
+}
