8f925fee180a501f3adbb2b8cca93e5a2050fd99
AayushMohan <aayushmohan1702@gmail.com>
2022-02-13 20:15:35 +0530

    Added Transaction Provider

--- context/TransactionContext.js
+++ context/TransactionContext.js
@@ -8,13 +8,14 @@ if (typeof window !== 'undefined') {
   eth = window.ethereum
 }
 
-const TransactionProvider = ({children}) => {}
+const TransactionProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState()
 
-const connectWallet = async (metamask = eth) => {
-  try{
-   if (!metamask) return alert('Please install metamask')
-   const accounts = await metamask.request({method:'eth_requestAccounts'})
-   setCurrentAccount(accounts[0])
- }
- 
-};
+  const connectWallet = async (metamask = eth) => {
+    try {
+      if (!metamask) return alert('Please install metamask')
+      const accounts = await metamask.request({ method: 'eth_requestAccounts' })
+      setCurrentAccount(accounts[0])
+    } catch {}
+  }
+}
