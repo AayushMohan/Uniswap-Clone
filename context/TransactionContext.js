a93bfd4b62a716953863a50b6b330eb7166fe1bd
AayushMohan <aayushmohan1702@gmail.com>
2022-02-15 11:10:36 +0530

    Added Get Ethereum Contract

--- context/TransactionContext.js
+++ context/TransactionContext.js
@@ -8,6 +8,18 @@ if (typeof window !== 'undefined') {
   eth = window.ethereum
 }
 
+const getEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum)
+  const signer = provider.getSigner()
+  const transactionContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  )
+
+  return transactionContract
+}
+
 export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState()
   const [isLoading, setIsLoading] = useState(false)
@@ -109,7 +121,13 @@ export const TransactionProvider = ({ children }) => {
 
   return (
     <TransactionContext.Provider
-      value={{ currentAccount, connectWallet, sendTransaction, handleChange }}
+      value={{
+        currentAccount,
+        connectWallet,
+        sendTransaction,
+        handleChange,
+        formData,
+      }}
     >
       {children}
     </TransactionContext.Provider>
