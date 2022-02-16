cc7b0fabcaee0607b00fc001b65b02a6e7cf9649
AayushMohan <aayushmohan1702@gmail.com>
2022-02-16 14:20:07 +0530

    Added Contract ABI, Contract Address and Ethers

--- context/TransactionContext.js
+++ context/TransactionContext.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import { contractABI, contractAddress } from '../lib/constants'
+import { ethers } from '../ether'
 
 export const TransactionContext = React.createContext()
 
