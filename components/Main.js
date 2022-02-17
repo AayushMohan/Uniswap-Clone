457ad1d5b4f4e03d85988bc2ed3adfb2431f083d
AayushMohan <aayushmohan1702@gmail.com>
2022-02-17 22:43:29 +0530

    Added Main

--- components/Main.js
+++ components/Main.js
@@ -2,9 +2,13 @@ import Image from 'next/image'
 import { RiSettings3Fill } from 'react-icons/ri'
 import { AiOutlineDown } from 'react-icons/ai'
 import ethLogo from '../assets/eth.png'
-import { TransactionContext } from '../context/TransactionContext'
 import { useContext } from 'react'
-// import { useRouter } from 'next/router'
+import { TransactionContext } from '../context/TransactionContext'
+import Modal from 'react-modal'
+import { useRouter } from 'next/router'
+import TransactionLoader from './TransactionLoader'
+
+Modal.setAppElement('#__next')
 
 const style = {
   wrapper: `w-screen flex items-center justify-center mt-14`,
@@ -20,15 +24,32 @@ const style = {
   confirmButton: `bg-[#2172E5] my-2 rounded-2xl py-6 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-[#2172E5] hover:border-[#234169]`,
 }
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#0a0b0d',
+    padding: 0,
+    border: 'none',
+  },
+  overlay: {
+    backgroundColor: 'rgba(10, 11, 13, 0.75)',
+  },
+}
+
 const Main = () => {
   const { formData, handleChange, sendTransaction } =
     useContext(TransactionContext)
+  const router = useRouter()
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: any) => {
     const { addressTo, amount } = formData
     e.preventDefault()
 
-    if (addressTo || amount) return
+    if (!addressTo || !amount) return
 
     sendTransaction()
   }
@@ -70,10 +91,13 @@ const Main = () => {
           <div className={style.currencySelector}></div>
         </div>
         <div onClick={(e) => handleSubmit(e)} className={style.confirmButton}>
-          {' '}
           Confirm
         </div>
       </div>
+
+      <Modal isOpen={!!router.query.loading} style={customStyles}>
+        <TransactionLoader />
+      </Modal>
     </div>
   )
 }
